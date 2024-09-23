a2d712af93b02582c5c970526650335f5ffd77f6
Elijah Echekwu <elijahechekwu30@gmail.com>
2024-09-23 14:23:14 +0100

    Included user routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { Request, Response } from "express";
 import authRouter from "./routers/auth";
 import { errorHandler, routeNotFound } from "./middlewares"
+import userRouter from "./routers/user";
 
 const app = express();
 app.use(express.json())
@@ -13,7 +14,9 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Hello world");
 });
 
+app.use("/api", userRouter)
 app.use("/api/auth", authRouter)
+
 app.use(routeNotFound);
 app.use(errorHandler);
 
