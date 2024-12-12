56a5962f2f56637e569e81883deba9bee0a24e2c
Elijah Echekwu <elijahechekwu30@gmail.com>
2024-12-12 14:11:27 +0100

    updated mongoose against search injection vulnerability

--- src/controllers/courseController.ts
+++ src/controllers/courseController.ts
@@ -1,11 +1,13 @@
 import courseService from "../services/courseService";
 import { Request, Response, NextFunction } from "express";
+import asyncControllerWrapper from "../utils/asyncWrapper";
 import cloudinary from "../utils/cloudinary";
 import { HttpError } from "../middlewares";
 import { Types } from "mongoose";
 import Course from "../models/course";
 
-export async function course(req: Request, res: Response, next: NextFunction){
+
+export const course = asyncControllerWrapper(async (req: Request, res: Response, next: NextFunction)=>{
 
     const page =  Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
@@ -34,43 +36,32 @@ export async function course(req: Request, res: Response, next: NextFunction){
         courses,
         totalItems
     })
-}
+})
 
 
-export async function createCourse(req: Request, res: Response, next: NextFunction){
+export const createCourse = asyncControllerWrapper(async(req: Request, res: Response, next: NextFunction)=>{
 
+    const secureUrl = req.file.path;
+    const publicId = req.file.filename;
+    const instructorId = req.user._id;
+    const {courseCode , title, description}= req.body as {courseCode: string, title: string, description: string}
+    const payload = {courseCode, title, description, instructorId, secureUrl, publicId }
 
-    try{
-        const secureUrl = req.file.path;
-        const publicId = req.file.filename;
-        const instructorId = req.user._id;
-        const {courseCode , title, description}= req.body as {courseCode: string, title: string, description: string}
-        const payload = {courseCode, title, description, instructorId, secureUrl, publicId }
+    const course = await courseService.createCourse(payload);
 
-        const course = await courseService.createCourse(payload);
+    return res.status(201).json({status: true, message: "course created successfully ", course});
 
-        return res.status(201).json({status: true, message: "course created successfully ", course});
+})
 
-    }catch(error){
-        next(error)
-    }
-}
-
-export async function updateCourse(req: Request, res: Response, next: NextFunction){
-
+export const updateCourse = asyncControllerWrapper(async (req: Request, res: Response, next: NextFunction)=>{
   
     const {title, description} = req.body
     const {courseId} = req.params;
 
     const payload = {title, description, courseId}
-    try{
 
-        const updatedCourse = await courseService.updateCourse(payload)
-        return res.status(201).json({status: true, message: "course updated successfully ", updatedCourse});
+    const updatedCourse = await courseService.updateCourse(payload)
+    return res.status(201).json({status: true, message: "course updated successfully ", updatedCourse});
 
-    }catch (error){
-        next(error)
-    }
-
-}
+})
 
