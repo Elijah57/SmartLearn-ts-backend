e416761418d53f57c9630a53a3c4746eaa9d3c86
Elijah Echekwu <elijahechekwu30@gmail.com>
2025-02-03 16:35:43 +0100

    moved db.ts to configs directory

--- src/configs/redis.ts
+++ src/configs/redis.ts
@@ -2,12 +2,16 @@ import {Redis} from "ioredis"
 import { RedisStore } from "connect-redis"
 import config from "."
 
-export const connectRedisClient = ()=>{
-    return new Redis(config.redisUrl)
+export const connectRedisClient = () : Redis=>{
+    return new Redis(config.redisUrl, {maxRetriesPerRequest: null})
 }
+
+const connection = new Redis(config.redisUrl, {maxRetriesPerRequest: null})
 
 let redisClient = connectRedisClient();
 
 export const redisStore = new RedisStore({
     client: redisClient
 })
+
+export default connection
