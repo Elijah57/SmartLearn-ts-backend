531af1fd1780bfa2ce0d5e33f60888f0ee8ba19a
Elijah Echekwu <elijahechekwu30@gmail.com>
2025-03-10 11:20:07 +0100

    refactoring

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from "express";
-import User from "../models/users";
 import * as jwt from "jsonwebtoken";
 import config from "../configs";
 import { AuthenticatedRequest } from "../types";
