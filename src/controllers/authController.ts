15824e84ec6eb86a98fb5c416c8781f55d71014e
Elijah Echekwu <elijahechekwu30@gmail.com>
2025-03-10 10:56:28 +0100

    refactoring

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -15,11 +15,8 @@ export const register = asyncWrapper(async  (req:Request, res: Response, next: N
     if(req.body.password == null || req.body.email === null) throw new BadRequest("All fields are required")
     const {message, user} = await authService.signup(req.body)
     await userService.UserProfileInit(user)
-    // const log = {
-    //     level: "info",
-    //     message: `User created: ${user}` as string
-    // }
-    // publishLogs(log)
+    
+    publishLogs({level: "info", message: `User created: ${user}`})
     res.status(201).json({status: true, message, userId: user})
 })
 
