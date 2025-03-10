d27619bd696fa1f344fab476cc0e2e237402d038
Elijah Echekwu <elijahechekwu30@gmail.com>
2025-03-10 10:44:24 +0100

    refactoring

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,10 +3,16 @@ import userService  from "../services/userService";
 import {NextFunction, Request, Response} from "express"
 import asyncWrapper from "../utils/asyncWrapper";
 import { publishLogs } from "../queues/producers";
+import { BadRequest } from "../middlewares";
+
+
+
+
+
 
 // it signup the user
 export const register = asyncWrapper(async  (req:Request, res: Response, next: NextFunction)=>{
-
+    if(req.body.password == null || req.body.email === null) throw new BadRequest("All fields are required")
     const {message, user} = await authService.signup(req.body)
     await userService.UserProfileInit(user)
     // const log = {
@@ -17,42 +23,58 @@ export const register = asyncWrapper(async  (req:Request, res: Response, next: N
     res.status(201).json({status: true, message, userId: user})
 })
 
+
+
+
+
+
 // it logs in the user
 export const login = asyncWrapper(async (req: Request, res: Response, next: NextFunction)=>{
     const { user, accessToken } = await authService.login(req.body);
     res.status(200).json({status:true, data: {accessToken, user}})
 })
 
+
+
+
+
+
 // it process user email validation, from verification mail sent
 export const verifyEmail = asyncWrapper(async (req:Request, res:Response, next:NextFunction)=>{
-
     const token = req.query.token as string;
     const {status, message} = await authService.verify(token)
     res.status(200).json({status: status, message: message})
 })
 
-export const sendVerificationMail = asyncWrapper(async (req: Request, res: Response, next: NextFunction)=>{
 
+
+
+
+
+export const sendVerificationMail = asyncWrapper(async (req: Request, res: Response, next: NextFunction)=>{
     const email = req.user.email;
     const send = await authService.sendVerificationMail(email);
     res.status(200).json({message: "Verification Mail Sent"})
-
 })
 
-export const fogotPassword = asyncWrapper(async (req: Request, res: Response, next: NextFunction)=>{
 
+
+
+
+export const fogotPassword = asyncWrapper(async (req: Request, res: Response, next: NextFunction)=>{
     const email = req.body.email;
-    const send = await authService.fogotPassword(email);
+    await authService.fogotPassword(email);
     res.status(200).json({message: "password reset link sent"})
 })
 
 
+
+
+
 export const resetPassword = asyncWrapper(async(req: Request, res:Response, next: NextFunction)=>{
-
-        const token = req.query.token as string;
-        const {password} = req.body;
-        const payload = {resetToken: token, newPassword: password}
-        const resetMsg = await authService.resetPassword(payload);
-        res.status(200).json({status: true, message: resetMsg})
-
+    const token = req.query.token as string;
+    const {password} = req.body;
+    const payload = {resetToken: token, newPassword: password}
+    const resetMsg = await authService.resetPassword(payload);
+    res.status(200).json({status: true, message: resetMsg})
 })
